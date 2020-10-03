48500202b24b26a968086434a700e5122a510ec9
史春阳 <shichunyang02@meituan.com>
2020-10-03 11:41:06 +0800

    AbstractAcknowledgingMessageListener.java

--- src/main/java/com/scy/kafka/listener/AbstractAcknowledgingMessageListener.java
+++ src/main/java/com/scy/kafka/listener/AbstractAcknowledgingMessageListener.java
@@ -1,6 +1,9 @@
 package com.scy.kafka.listener;
 
+import com.scy.core.StringUtil;
+import com.scy.core.format.MessageUtil;
 import com.scy.core.trace.TraceUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.AcknowledgingMessageListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -11,14 +14,28 @@ import org.springframework.kafka.support.Acknowledgment;
  * @author shichunyang
  * Created by shichunyang on 2020/10/2.
  */
+@Slf4j
 public abstract class AbstractAcknowledgingMessageListener implements AcknowledgingMessageListener<String, String> {
 
     @Override
     public void onMessage(ConsumerRecord<String, String> consumerRecord, Acknowledgment acknowledgment) {
+        TraceUtil.setTraceId(null);
+
+        String topic = consumerRecord.topic();
+        String key = consumerRecord.key();
+        String value = consumerRecord.value();
+        int partition = consumerRecord.partition();
+        long offset = consumerRecord.offset();
         try {
-            TraceUtil.setTraceId(null);
+            long startTime = System.currentTimeMillis();
 
             listen(consumerRecord);
+
+            log.info(MessageUtil.format("kafka listen",
+                    "topic", topic, "key", key, "value", value, "partition", partition, "offset", offset, StringUtil.COST, System.currentTimeMillis() - startTime));
+        } catch (Throwable throwable) {
+            log.error(MessageUtil.format("kafka listen error", throwable,
+                    "topic", topic, "key", key, "value", value, "partition", partition, "offset", offset));
         } finally {
             TraceUtil.clearTrace();
             acknowledgment.acknowledge();
