23446c1246f9cf9ec4eef1298b7da30e4aefa052
shichunyang02 <shichunyang02@meituan.com>
2024-01-29 17:23:08 +0800

    支持事务

--- src/main/java/com/scy/kafka/util/KafkaProducer.java
+++ src/main/java/com/scy/kafka/util/KafkaProducer.java
@@ -10,10 +10,6 @@ import org.springframework.lang.NonNull;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 /**
  * KafkaProducer
  *
@@ -28,32 +24,30 @@ public class KafkaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String key, String value) throws ExecutionException {
+    public Boolean send(String key, String value) {
         String traceId = TraceUtil.getTraceId();
 
-        ListenableFuture<SendResult<String, String>> listenableFuture = kafkaTemplate.send(topic, key, value);
-        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+        return kafkaTemplate.executeInTransaction(operations -> {
+            ListenableFuture<SendResult<String, String>> listenableFuture = kafkaTemplate.send(topic, key, value);
+            listenableFuture.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+                @Override
+                public void onFailure(@NonNull Throwable throwable) {
+                    TraceUtil.putMdc(TraceUtil.TRACE_ID, traceId);
+                    log.error(MessageUtil.format("kafka send error", throwable, "topic", topic, "key", key, "value", value));
+                    TraceUtil.clearMdc();
+                }
 
-            @Override
-            public void onFailure(@NonNull Throwable throwable) {
-                TraceUtil.putMdc(TraceUtil.TRACE_ID, traceId);
-                log.error(MessageUtil.format("kafka send error", throwable, "topic", topic, "key", key, "value", value));
-                TraceUtil.clearMdc();
-            }
+                @Override
+                public void onSuccess(SendResult<String, String> result) {
+                    TraceUtil.putMdc(TraceUtil.TRACE_ID, traceId);
+                    log.info(MessageUtil.format("kafka send success", "topic", topic, "key", key, "value", value));
+                    TraceUtil.clearMdc();
+                }
+            });
 
-            @Override
-            public void onSuccess(SendResult<String, String> result) {
-                TraceUtil.putMdc(TraceUtil.TRACE_ID, traceId);
-                log.info(MessageUtil.format("kafka send success", "topic", topic, "key", key, "value", value));
-                TraceUtil.clearMdc();
-            }
+            listenableFuture.completable().join();
+
+            return Boolean.TRUE;
         });
-        try {
-            listenableFuture.get(5, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            log.warn(MessageUtil.format("kafka send interrupted", "topic", topic, "key", key, "value", value));
-        } catch (TimeoutException e) {
-            log.warn(MessageUtil.format("kafka send timeout", "topic", topic, "key", key, "value", value));
-        }
     }
 }
